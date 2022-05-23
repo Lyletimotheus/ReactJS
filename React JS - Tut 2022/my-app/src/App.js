842078a4cb6446563450cf221f95d23a687f2e74
Lyletimotheus <lyle6494@gmail.com>
2022-05-23 14:14:04 +0200

    Package updates

--- React JS - Tut 2022/my-app/src/App.js
+++ React JS - Tut 2022/my-app/src/App.js
@@ -1,13 +1,39 @@
-// PROJECT 1 IMPORTS 
-import './project_1/App.css';
-import Header from './project_1/components/Header';
-import Page from './project_1/components/Page';
+// ------------------------------------ IMPORTS START ------------------------------------
+// *** PROJECT 1 IMPORTS ***
+// import './project_1/App.css';
+// import Header from './project_1/components/Header';
+// import Page from './project_1/components/Page';
 
+// *** PROJECT 2 IMPORTS ***
+import Info from './project_2/components/Info';
+import About from './project_2/components/About';
+import Interests from './project_2/components/Interests';
+import Footer from './project_2/components/Footer';
+import './project_2/App.css';
+// ------------------------------------ IMPORTS END --------------------------------------
+
+
+
+// PROJECT 1 FUNCTION
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Page />
+//     </div>
+//   );
+// }
+
+// PROJECT 2 FUNCTION
 function App() {
   return (
-    <div>
-      <Header />
-      <Page />
+    <div className="card-container">
+      <main>
+        < Info />
+        < About />
+        < Interests />
+        < Footer />
+      </main>
     </div>
   );
 }
