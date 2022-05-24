2dd649d5e2bcde804b8b0f369241e4bd085fd613
Lyletimotheus <lyle6494@gmail.com>
2022-05-24 10:58:45 +0200

    project UI completed - static assets

--- React JS - Tut 2022/my-app/src/project_3/components/Card.js
+++ React JS - Tut 2022/my-app/src/project_3/components/Card.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import card_image from '../assets/image_12.png';
+import star from '../assets/star.svg';
 
 function Card() {
   return (
-    <section>
-        
+    <section className='cards'>
+        <div className='card-container'>
+          <img src={card_image} alt="katie zaferes"/>
+          <div className='card-body'>
+            <p>
+              <img src={star} alt='star icon' className='card-body--star'/> 5.0 
+              <span className='card-body--text_top'>(6) &#9679; USA </span>
+            </p>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className='bold'>From $136</span> / person</p>
+          </div>
+        </div>
     </section>
   )
 }
