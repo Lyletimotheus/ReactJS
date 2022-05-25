626e9c96530fe7debcb0a593b75f437fcf013a69
Lyletimotheus <lyle6494@gmail.com>
2022-05-25 11:31:19 +0200

    Project 3 cards displaying

--- React JS - Tut 2022/my-app/src/App.js
+++ React JS - Tut 2022/my-app/src/App.js
@@ -16,7 +16,11 @@ import Header from './project_3/components/Header';
 import Hero from './project_3/components/Hero';
 import Card from './project_3/components/Card';
 import './project_3/App.css';
-
+import cardImage1 from './project_3/assets/image_12.png';
+import cardImage2 from './project_3/assets/image_13.png';
+import cardImage3 from './project_3/assets/image_14.png';
+import cardImage4 from './project_3/assets/image_15.jpg';
+import cardImage5 from './project_3/assets/image_16.jpg';
 // ------------------------------------ IMPORTS END --------------------------------------
 
 
@@ -52,7 +56,50 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <Card />
+        <section className='cards'>
+          <Card 
+            isSoldOut = {true}
+            img = {cardImage1}
+            rating = {5.0}
+            ratingTotal = {6}
+            country = "USA"
+            text = "Life lessons with Katie Zaferes"
+            amount = {136}
+          />
+          <Card 
+            status  = {true}
+            img = {cardImage2}
+            rating = {5.0}
+            ratingTotal = {30}
+            country = "USA"
+            text = "Learn wedding photography"
+            amount = {125}
+          />
+          <Card
+            img = {cardImage3}
+            rating = {4.8}
+            ratingTotal = {2}
+            country = "USA"
+            text = "Group Mountain Biking"
+            amount = {50}
+          />
+          <Card
+            img = {cardImage4}
+            rating = {3.5}
+            ratingTotal = {19}
+            country = "USA"
+            text = "Group Coffee Mastery"
+            amount = {150}
+          />
+          <Card 
+            img = {cardImage5}
+            rating = {5.0}
+            ratingTotal = {209}
+            country = "USA"
+            text = "Skydiving"
+            amount = {240}
+          />
+        </section>
       </main>
     </div>
   );
--- React JS - Tut 2022/my-app/src/project_3/components/Card.js
+++ React JS - Tut 2022/my-app/src/project_3/components/Card.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import card_image from '../assets/image_12.png';
+// import card_image from '../assets/image_12.png';
 import star from '../assets/star.svg';
 
-function Card() {
+function Card({isSoldOut,status, img, rating, ratingTotal, country, text, amount}) {
+  console.log(isSoldOut);
   return (
-    <section className='card'>
+    <div className='card'>
         <div className='card-container'>
-          <img src={card_image} alt="katie zaferes"/>
-          <span>SOLD OUT</span>
+          <img src={img} alt="katie zaferes" width="264" height="353" className="card--image"/>
+          <span className={isSoldOut ? 'SOLD OUT' : ""}></span>
 
           <div className='card-body'>
             <p>
-              <img src={star} alt='star icon' className='card-body--star'/> 5.0 
-              <span className='card-body--text_top'>(6) &#9679; USA </span>
+              <img src={star} alt='star icon' className='card-body--star'/> {rating} 
+              <span className='card-body--text_top'>({ratingTotal}) &#9679; {country} </span>
             </p>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className='bold'>From $136</span> / person</p>
+            <p>{text}</p>
+            <p><span className='bold'>From ${amount}</span> / person</p>
           </div>
         </div>
-    </section>
+    </div>
   )
 }
 
