caa895b9a41fef7ead30289e9c3300e7e0f009ce
Lyletimotheus <lyle6494@gmail.com>
2022-05-28 07:16:43 +0200

    Added images using state

--- React JS - Tut 2022/my-app/src/project_5/components/Meme.js
+++ React JS - Tut 2022/my-app/src/project_5/components/Meme.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
+import memesData from '../memesData'
 
 function Meme() {
+    const [memeImage, setMemeImage] = React.useState("");
+    // console.log(memeImage);
+
+    function newImage() {
+        const memeArray = memesData.data.memes;
+        const randomImage = memeArray[Math.floor(Math.random() * memeArray.length)];
+        // console.log(randomImage.url);       
+        
+        return setMemeImage(randomImage.url);
+
+    }
+
+    
   return (
-    <section>
-        <form>
-            <input type="text" placeholder='please enter top text'/>
-            <input type="text" placeholder='please enter bottom text'/>
-            <button>Get a new meme image 🖼</button> 
-        </form>
+    <section className="form--section">
+        <div className="form">
+            <input type="text" placeholder='please enter top text' className="form--input"/>
+            <input type="text" placeholder='please enter bottom text' className="form--input"/>
+        </div>
+        <button onClick={newImage}>Get a new meme image 🖼</button> 
+
+        <img src={memeImage} alt="" className="memeImage"/>
+        
     </section>
   )
 }
